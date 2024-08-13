18d480d655021515287a4b37ce3060cb6399d03a
Esau Garcia <esautfu@gmail.com>
2024-08-13 04:22:55 -0500

    d

--- server.js
+++ server.js
@@ -9,7 +9,6 @@ import messageRoutes from "./routes/messageRoutes.js";
 import { v2 as cloudinary } from "cloudinary";
 import { app, server } from "./socket/socket.js";
 import job from "./cron/cron.js";
-import cors from "cors"; // Importa cors
 
 dotenv.config();
 
@@ -25,13 +24,13 @@ cloudinary.config({
 	api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
-// Middlewares
 app.use(cors({
     origin: "*",
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
 }));
 
+// Middlewares
 app.use(express.json({ limit: "50mb" })); // To parse JSON data in the req.body
 app.use(express.urlencoded({ extended: true })); // To parse form data in the req.body
 app.use(cookieParser());
@@ -41,7 +40,10 @@ app.use("/api/users", userRoutes);
 app.use("/api/posts", postRoutes);
 app.use("/api/messages", messageRoutes);
 
-// Serve frontend in production
+
+
+// http://localhost:5000 => backend,frontend
+
 if (process.env.NODE_ENV === "production") {
 	app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
@@ -51,4 +53,3 @@ if (process.env.NODE_ENV === "production") {
 	});
 }
 
-server.listen(PORT, () => console.log(`Server started at http://localhost:${PORT}`));
