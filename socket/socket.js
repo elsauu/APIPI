46fc3580d95ed96aac070342462fca3983b406a5
Esau Garcia <esautfu@gmail.com>
2024-08-13 04:23:25 -0500

    ddddd

--- socket/socket.js
+++ socket/socket.js
@@ -7,10 +7,10 @@ import Conversation from "../models/conversationModel.js";
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
-	cors: {
-		origin: "https://apipi.onrender.com",
-		methods: ["GET", "POST"],
-	},
+    cors: {
+        origin: ["http://localhost:3000", "https://apipi.onrender.com"], // Agrega ambos orígenes
+        methods: ["GET", "POST"],
+    },
 });
 
 export const getRecipientSocketId = (recipientId) => {
@@ -20,27 +20,31 @@ export const getRecipientSocketId = (recipientId) => {
 const userSocketMap = {}; // userId: socketId
 
 io.on("connection", (socket) => {
-	console.log("user connected", socket.id);
-	const userId = socket.handshake.query.userId;
+    console.log("user connected", socket.id);
+    const userId = socket.handshake.query.userId;
 
-	if (userId != "undefined") userSocketMap[userId] = socket.id;
-	io.emit("getOnlineUsers", Object.keys(userSocketMap));
+    if (userId && userId !== "undefined") {
+        userSocketMap[userId] = socket.id;
+        io.emit("getOnlineUsers", Object.keys(userSocketMap));
+    } else {
+        console.log("Invalid userId received:", userId);
+    }
 
-	socket.on("markMessagesAsSeen", async ({ conversationId, userId }) => {
-		try {
-			await Message.updateMany({ conversationId: conversationId, seen: false }, { $set: { seen: true } });
-			await Conversation.updateOne({ _id: conversationId }, { $set: { "lastMessage.seen": true } });
-			io.to(userSocketMap[userId]).emit("messagesSeen", { conversationId });
-		} catch (error) {
-			console.log(error);
-		}
-	});
+    socket.on("markMessagesAsSeen", async ({ conversationId, userId }) => {
+        try {
+            await Message.updateMany({ conversationId: conversationId, seen: false }, { $set: { seen: true } });
+            await Conversation.updateOne({ _id: conversationId }, { $set: { "lastMessage.seen": true } });
+            io.to(userSocketMap[userId]).emit("messagesSeen", { conversationId });
+        } catch (error) {
+            console.log(error);
+        }
+    });
 
-	socket.on("disconnect", () => {
-		console.log("user disconnected");
-		delete userSocketMap[userId];
-		io.emit("getOnlineUsers", Object.keys(userSocketMap));
-	});
+    socket.on("disconnect", () => {
+        console.log("user disconnected");
+        delete userSocketMap[userId];
+        io.emit("getOnlineUsers", Object.keys(userSocketMap));
+    });
 });
 
 export { io, server, app };
